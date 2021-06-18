448d0408d14a21c08ff98b4e90a40b6b6b80729b
jose antonio figueroa aravena <jfigueroaa@udec.cl>
2021-06-17 20:33:35 -0400

    CAMBIOS EN EL PROYECTO

--- ProyectoFinalLpw/Controllers/FacturaController.cs
+++ ProyectoFinalLpw/Controllers/FacturaController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ProyectoFinalLpw.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +11,61 @@ namespace ProyectoFinalLpw.Controllers
 {
     public class FacturaController : Controller
     {
-        private VentasdbEntities db = VentasdbEnt
+
+        private VentasEntities1 db = new VentasEntities1();
+
         // GET: Factura
-        public ActionResult Factura()
+
+
+        public ActionResult Index()
+        {
+            var facturas = db.Facturas.ToList();
+            return View(facturas);
+        }
+        public ActionResult Create()
+
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(Facturas facturas)
+        {
+            db.Facturas.Add(facturas);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            Facturas facturas = db.Facturas.Find(id);
+            return PartialView("_Edit", facturas);
+        }
+        [HttpPost]
+        public ActionResult Edit(Facturas facturas)
+        {
+            db.Entry(facturas).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                var facturas = db.Facturas.Find(id);
+                return PartialView("_Delete", facturas);
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var facturas = db.Facturas.Find(id);
+            db.Facturas.Remove(facturas);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
+
+   
--- ProyectoFinalLpw/Controllers/ProductosController.cs
+++ ProyectoFinalLpw/Controllers/ProductosController.cs
@@ -1,17 +1,70 @@
-﻿using System;
+﻿using ProyectoFinalLpw.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
 namespace ProyectoFinalLpw.Controllers
 {
-    public class ProductosController : Controller
+    public class ProcuctosController : Controller
     {
-        // GET: Productos
-        public ActionResult Productos()
+
+        private VentasEntities1 db = new VentasEntities1();
+
+        // GET: Factura
+
+
+        public ActionResult Index()
+        {
+            var productos = db.Productos.ToList();
+            return View(productos);
+        }
+        public ActionResult Create()
+
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(Productos productos)
+        {
+            db.Productos.Add(productos);
+            db.SaveChanges();
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            Productos productos= db.Productos.Find(id);
+            return PartialView("_Edit", productos);
+        }
+        [HttpPost]
+        public ActionResult Edit(Productos productos)
+        {
+            db.Entry(productos).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id != null)
+            {
+                var productos = db.Facturas.Find(id);
+                return PartialView("_Delete", productos);
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var productos = db.Productos.Find(id);
+            db.Productos.Remove(productos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
+
